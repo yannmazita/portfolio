522e06c0c8067ec4081fd42c40cc25104f5f387d
Yann Mazita <dev.mazita@gmail.com>
2025-06-25 18:12:52 +0200

    Lightning style update

--- src/features/projects/components/ProjectsSection.tsx
+++ src/features/projects/components/ProjectsSection.tsx
@@ -6,15 +6,35 @@ import { random } from "@/common/utils/math";
 export const ProjectsSection: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useLightning(canvasRef, {
+    minDelay: 10,
+    maxDelay: 10,
+    minCreateCount: 4,
+    maxCreateCount: 4,
+    trailLength: 10,
+    minLineWidth: 1,
+    maxLineWidth: 10,
+    minSpeed: 40,
+    maxSpeed: 60,
+    /*
     minCreateCount: 10,
     maxCreateCount: 30,
     trailLength: 30,
     minLineWidth: 1,
     maxLineWidth: 15,
+    minSpeed: 20,
+    maxSpeed: 30,
+    */
+
+    startPosition: (w, h) => ({
+      x: w / 2 + random(-w / 15, w / 15),
+      y: h / 2 + h / 7,
+    }),
+    /*
     startPosition: (w, h) => ({
       x: w / 2 + random(-w / 10, w / 10),
       y: h / 2 + h / 7,
     }),
+    */
     startVelocity: () => ({
       vx: random(-1, 1),
       vy: 1,
