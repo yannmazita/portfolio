1df7dcad7f9992d36afdcab64a3c9717525f1f22
Yann Mazita <dev.mazita@gmail.com>
2025-07-05 23:35:31 +0200

    Non-root page refresh workaround

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 const ReactCompilerConfig = {
   /* ... */
@@ -20,6 +21,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // GitHub Pages workaround for non-root page refresh
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "404": resolve(__dirname, "index.html"),
+      },
     },
   },
 });
