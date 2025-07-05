aa174eb3d2a8426b54b29c44d446e7d8fa3f22c2
Yann Mazita <dev.mazita@gmail.com>
2025-07-05 23:45:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
 
 const ReactCompilerConfig = {
   /* ... */
