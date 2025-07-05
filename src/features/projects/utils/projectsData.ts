a9a9a52de58477a89fbf8a7485b877a2da894bfa
Yann Mazita <dev.mazita@gmail.com>
2025-07-05 12:14:25 +0200

    Added project

--- src/features/projects/utils/projectsData.ts
+++ src/features/projects/utils/projectsData.ts
@@ -50,6 +50,15 @@ export function projectsData(): PortfolioProject[] {
         "Display components in movable and resizable 'windows'. You can also minimize and maximize them.",
     },
     {
+      name: "This website :)",
+      mediaUrl: "some/link",
+      projectLink: "mazita.com",
+      techStack: ["React", "Typescript", "CSS", "TailwindCSS", "React-Router"],
+      shortDescription: "You're currently browsing it.",
+      description:
+        "Drawing inspiration specifically from the early 2000s era of fast and nimble websites, Flash animations and Electroclash music.",
+    },
+    {
       name: "step.ts",
       mediaUrl: "some/link",
       projectLink: "https://www.github.com/yannmazita/step.ts",
