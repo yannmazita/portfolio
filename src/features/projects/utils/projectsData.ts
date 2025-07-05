288419cb111e88c30b1d3dc7d1fba8bc83275f5a
Yann Mazita <dev.mazita@gmail.com>
2025-07-05 23:58:10 +0200

    Added link to portfolio repo

--- src/features/projects/utils/projectsData.ts
+++ src/features/projects/utils/projectsData.ts
@@ -53,7 +53,7 @@ export function projectsData(): PortfolioProject[] {
     {
       name: "This website :)",
       mediaUrl: "some/link",
-      projectLink: "mazita.dev",
+      projectLink: "https://www.github.com/yannmazita/portfolio",
       techStack: ["React", "Typescript", "CSS", "TailwindCSS", "React-Router"],
       shortDescription: "You're currently browsing it.",
       description:
