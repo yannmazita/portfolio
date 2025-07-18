8607b466372b79e415039abdd809c1c83237aabd
Yann Mazita <dev.mazita@gmail.com>
2025-07-18 18:17:53 +0200

    Updated filepath

--- src/features/resume/components/ResumeSection.tsx
+++ src/features/resume/components/ResumeSection.tsx
@@ -40,7 +40,7 @@ export const ResumeSection: React.FC = () => {
           Service Record
         </h2>
         <a
-          href={getAssetUrl("/resume/cv_mazita_yann.pdf")}
+          href={getAssetUrl("/resume/mazita_yann_cv.pdf")}
           target="_blank"
           rel="noopener noreferrer"
           className={cn(
