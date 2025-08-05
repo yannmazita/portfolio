2a80db4a49897fe90f7718d545d1557694834a9d
Yann Mazita <dev.mazita@gmail.com>
2025-08-05 11:15:29 +0200

    Implemented easing functions

--- src/common/utils/math.ts
+++ src/common/utils/math.ts
@@ -1,5 +1,4 @@
 // src/common/utils/math.ts
-
 export const random = (min: number, max: number): number =>
   Math.random() * (max - min) + min;
 
@@ -9,3 +8,10 @@ export const jitter = (
   max: number,
   variation: number,
 ) => Math.max(min, Math.min(max, value + random(-variation, variation)));
+
+export const easingFunctions = {
+  linear: (t: number) => t,
+  easeOut: (t: number) => 1 - Math.pow(1 - t, 3),
+  easeInOut: (t: number) =>
+    t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2,
+};
